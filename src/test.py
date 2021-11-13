b5b6aa41317a09b145864c676df0acec3fef428b
bryanteoh964 <68241796+bryanteoh964@users.noreply.github.com>
2021-11-13 16:51:17 -0500

    Test.py: Added icon

--- src/test.py
+++ src/test.py
@@ -14,6 +14,7 @@ def deleteTask(event=None):
     
 ws = Tk()
 ws.geometry('500x450+500+200')
+ws.iconbitmap('assets\CWIcon.ico')
 ws.title('Chillwork')
 ws.config(bg='#223441')
 ws.resizable(width=False, height=False)
